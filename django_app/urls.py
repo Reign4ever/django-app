524fb5e05a09334425b48ee5b3a642d9ccfda286
Reign4ever <for31gn@yahoo.com>
2026-04-16 16:23:25 -0700

    Fix homepage redirect

--- django_app/urls.py
+++ django_app/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('django_app.api.urls')),
-    path('', lambda request: redirect('api/blogposts/')),
+    path('', RedirectView.as_view(url='/api/blogposts/')),
 ]
