65c7ec71986d77c0b90fcc8049670f049c353d98
Reign4ever <for31gn@yahoo.com>
2026-04-16 16:15:37 -0700

    Redirect homepage to api/blogposts

--- django_app/urls.py
+++ django_app/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('django_app.api.urls')),
+    path('', lambda request: redirect('api/blogposts/')),
 ]
