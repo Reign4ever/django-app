82b24fb35f473f2c1a0b5a779ac93c31f75fdca1
Reign4ever <for31gn@yahoo.com>
2026-04-28 14:39:27 -0700

    Add token authentication endpoint

--- django_app/urls.py
+++ django_app/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('django_app.api.urls')),
+    path('api/token/', obtain_auth_token, name='api_token_auth'),
     path('', RedirectView.as_view(url='/api/')),
 ]
 
