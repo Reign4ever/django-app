08a9507569c883a095c400ef87fc9b0ad92ee099
Reign4ever <for31gn@yahoo.com>
2026-05-12 20:04:42 -0700

    Add TokenAuthentication to EventRetrieveUpdateDestroy

--- django_app/api/views.py
+++ django_app/api/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.authentication import TokenAuthentication
 from .models import UserProfile, Event
 from .serializers import UserProfileSerializer, EventSerializer
-from rest_framework.permissions import IsAuthenticated, AllowAny
+
 
 class UserProfileListCreate(generics.ListCreateAPIView):
     queryset = UserProfile.objects.all()
@@ -13,10 +15,12 @@ class UserProfileListCreate(generics.ListCreateAPIView):
         UserProfile.objects.all().delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
 class UserProfileRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     lookup_field = "pk"
+
 
 def index(request):
     return render(request, 'index.html')
@@ -35,6 +39,7 @@ class EventListCreate(generics.ListCreateAPIView):
 
 class EventRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = EventSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
