90ccfbeaa11e888154b463a025db68dd4e627b38
Reign4ever <for31gn@yahoo.com>
2026-06-03 17:08:56 -0700

    Fix Yahoo SMTP to use SSL port 465

--- django_app/settings.py
+++ django_app/settings.py
@@ -100,8 +100,9 @@ CACHES = {
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mail.yahoo.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER', '')
