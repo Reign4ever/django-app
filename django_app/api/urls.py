04af1da1621e0cf04f19cdb6f50695b93e8184eb
Reign4ever <for31gn@yahoo.com>
2026-07-10 20:25:04 -0700

    Add MOBILE_API_SECRET

--- django_app/api/urls.py
+++ django_app/api/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path("register/", views.RegisterView.as_view(), name="register"),
+    path("mobile/token/", views.MobileLoginView.as_view(), name="mobile-login"),
+    path("mobile/register/", views.MobileRegisterView.as_view(), name="mobile-register"),
     path("change-password/", views.ChangePasswordView.as_view(), name="change-password"),
     path("forgot-password/", views.ForgotPasswordView.as_view(), name="forgot-password"),
     path("reset-password/", views.ResetPasswordView.as_view(), name="reset-password"),
